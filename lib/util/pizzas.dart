5599f7a6c45b789ba4c39be3c05229876ae43ce5
crosswjchoi <wjchoi@crosscert.com>
2021-01-15 14:53:36 +0900

    Added new assets

--- lib/util/pizzas.dart
+++ lib/util/pizzas.dart
@@ -1,36 +1,36 @@
 List pizzas = [
   {
-    "img": "assets/cheese_pizza.png",
+    "img": "assets/cheese_pizza.jpg",
     "title": "Classic Cheese",
     "address": "Mozzarella, Provolone, Cheddar, Parmesan, Romano cheese",
     "rating": "4.9"
   },
   {
-    "img": "assets/bbq_pizza.jpeg",
+    "img": "assets/bbq_pizza.jpg",
     "title": "BBQ Chicken",
     "address": "Smoked cheese, BBQ sauce, Chicken breast",
     "rating": "4.3"
   },
   {
-    "img": "assets/pepperoni_pizza.jpeg",
+    "img": "assets/pepperoni_pizza.jpg",
     "title": "Pepperoni",
     "address": "Salami, Ham, Bacon",
     "rating": "4.5"
   },
   {
-    "img": "assets/hawaiian_pizza.jpeg",
+    "img": "assets/hawaiian_pizza.jpg",
     "title": "Hawaiian",
     "address": "Ham, Pineapple",
     "rating": "4.4"
   },
   {
-    "img": "assets/supreme_pizza.png",
+    "img": "assets/supreme_pizza.jpg",
     "title": "Supreme",
     "address": "Salami, Sausage, Mushroom, Green pepper",
     "rating": "4.9"
   },
   {
-    "img": "assets/veggie_pizza.jpeg",
+    "img": "assets/veggie_pizza.jpg",
     "title": "Veggie",
     "address": "Mushroom, Onion, Pepper, Eggplant, Olive, Broccoli",
     "rating": "4.3"
--- lib/util/sides.dart
+++ lib/util/sides.dart
@@ -5,36 +5,36 @@ List sides = [
     "name": "Fries",
     "color1": Color.fromARGB(100, 0, 0, 0),
     "color2": Color.fromARGB(100, 0, 0, 0),
-    "img": "assets/fries_side.jpeg"
+    "img": "assets/fries_side.jpg"
   },
   {
     "name": "Caesar Salad",
     "color1": Color.fromARGB(100, 0, 0, 0),
     "color2": Color.fromARGB(100, 0, 0, 0),
-    "img": "assets/salad_side.jpeg"
+    "img": "assets/salad_side.jpg"
   },
   {
     "name": "Spaghetti",
     "color1": Color.fromARGB(100, 0, 0, 0),
     "color2": Color.fromARGB(100, 0, 0, 0),
-    "img": "assets/spaghetti_side.jpeg"
+    "img": "assets/spaghetti_side.jpg"
   },
   {
     "name": "Wings",
     "color1": Color.fromARGB(100, 0, 0, 0),
     "color2": Color.fromARGB(100, 0, 0, 0),
-    "img": "assets/wings_side.jpeg"
+    "img": "assets/wings_side.jpg"
   },
   {
     "name": "Coke",
     "color1": Color.fromARGB(100, 0, 0, 0),
     "color2": Color.fromARGB(100, 0, 0, 0),
-    "img": "assets/coke_side.jpeg"
+    "img": "assets/coke_side.jpg"
   },
   {
-    "name": "Sprite",
+    "name": "Red Bull",
     "color1": Color.fromARGB(100, 0, 0, 0),
     "color2": Color.fromARGB(100, 0, 0, 0),
-    "img": "assets/sprite_side.jpeg"
+    "img": "assets/sprite_side.jpg"
   },
 ];
